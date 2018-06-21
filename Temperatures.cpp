e5b234a0de3de80d4ce7d0c450fad860480d118c
spindycell <31498152+spindycell@users.noreply.github.com>
2018-06-21 10:45:22 +0700

    update 1

---
+++ Temperatures.cpp
@@ -0,0 +1,26 @@
+// https://www.codingame.com/training/easy/temperatures
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+int main()
+{
+    int n; // the number of temperatures to analyse
+    cin >> n; cin.ignore();
+    for (int i = 0; i < n; i++) {
+        int t; // a temperature expressed as an integer ranging from -273 to 5526
+        cin >> t; cin.ignore();
+    }
+
+    // Write an action using cout. DON'T FORGET THE "<< endl"
+    // To debug: cerr << "Debug messages..." << endl;
+
+    cout << "result" << endl;
+}
